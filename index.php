99a6fe817724d147a958e90da0529223275f5d68
Wendy Salinas-Reyes <wendy3salinas@gmail.com>
2022-02-17 09:21:37 -0800

    adds files

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>PHP File!</title>
+</head>
+
+<body>
+    <h1>PHP File</h1>
+    <p>This is HTML!</p>
+
+    <?php
+        echo "<p>This is </p>";
+    
+    ?>
+
+</body>
+
+
+</html>
